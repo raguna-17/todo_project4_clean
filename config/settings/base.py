2c592c138b979441e277420c9d6b32196913102a
らぐな <pasupoto17@gmail.com>
2026-01-01 18:12:40 +0900

    kklkl

--- config/settings/base.py
+++ config/settings/base.py
@@ -24,18 +24,18 @@ DATABASES = {
 }
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 CORS_ALLOW_ALL_ORIGINS = True
 
--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,15 +1,17 @@
 from .base import *
 
-DEBUG = True
-
+DEBUG = False
 ALLOWED_HOSTS = ["todo-project4-clean.onrender.com"]
-
-STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://todo-project4-clean.onrender.com",
 ]
+
+# Whitenoise用の設定
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
